4203d542e8a25b9faab79294bae3f62d821bf649
felipegtech <felipegomezd26@outlook.com>
2025-06-12 10:54:47 -0500

    🚀 finding US numbers patterns

---
+++ isPhoneNumber.py
@@ -0,0 +1,20 @@
+def isPhoneNUmber(text):
+  if len(text) != 12:
+    return False
+  for i in range(0, 3):
+    if not text[i].isdecimal():
+      return False
+  if text[3] != '-':
+    return False
+  for i in range(4, 7):
+    if not text[i].isdecimal():
+      return False
+  if text[7] != '-':
+    return False
+  for i in range(8, 12):
+    if not text[i].isdecimal():
+      return False
+  return True
+
+print('Is 415-55-4242 a phone number?')
+print(isPhoneNUmber('415-555-4242'))
